3f925d6963ad60239a25e3671906f8e219ea5932
Danilenko Mike <mikexplorer@gmail.com>
2025-04-09 09:06:08 +0200

    added some methods to repository

---
+++ cmd/app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	repository "github.com/muhlikus/tgbot/internal/repository/external/telegram"
+
+	"github.com/muhlikus/telegramclient"
+)
+
+func main() {
+
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatalln("TELEGRAM_BOT_TOKEN environment must be set")
+	}
+
+	config := &Config{Token: botToken}
+
+	telegramClient, err := telegramclient.New(telegramclient.Config{Token: config.Token})
+	if err != nil {
+		log.Fatalf("Failed to create telegram client: %v", err)
+	}
+
+	rep, err := repository.NewRepository(telegramClient)
+	if err != nil {
+		log.Fatalf("Failed to create repository: %v", err)
+	}
+	_ = rep
+
+}
